1f6a030e4c6afdbca184e4fbcd22a51ea45ce2b6
adelesr <adelesr77@gmail.com>
2025-02-23 23:50:10 +0200

    creating both folders

---
+++ Backend/app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { config } from 'dotenv';
+
+const app = express();
+config();
+const port=process.env.PORT||3030;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
